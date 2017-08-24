24d4a2cf4f46acaf3f5008830810d5e9dd3d5d75
Peter Cheng <rhoxwarrior@gmail.com>
2017-08-24 03:57:18 -0400

    Modified dependencies, integrated expo with native client

--- client/App.js
+++ client/App.js
@@ -1,14 +1,29 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Alert } from 'react-native';
+import Expo from 'expo';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this._fbLogin = this._fbLogin.bind(this);
+  }
+
+  _fbLogin() {
+
+  }
+
   render() {
+    function testAlert() {
+      Alert.alert('Logged in!');
+      console.log('ok');
+    }
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-        <Text>Hello World!</Text>
+        <Text>This will be a map</Text>
+        <Text>Will there be hot reload</Text>
+        <TouchableOpacity onPress={ testAlert }>
+          <Text>Login with Facebook</Text>
+        </TouchableOpacity>
       </View>
     );
   }
